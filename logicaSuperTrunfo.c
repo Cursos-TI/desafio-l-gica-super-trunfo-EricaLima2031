b82ab811b3eb51ab94fa1723b3420cf9dacfb303
EricaLima2031 <ericalima2031@gmail.com>
2025-01-30 15:48:12 +0000

    Desenvolvendo a logica do jogo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,10 +6,24 @@ int main() {
 
 
 char nome [50];
-char estador [50];
+char estado [50];
 int codigo;
 int populacao;
 int area;
 double pib;
 
 } Cartas;
+
+printf (" Digite o nome da Cidade! ");
+scanf ("%s", nome, 50);
+scanf ("%s", estado, 50);
+printf (" Digite o codigo da Cidade: ");
+scanf ("%d", &codigo);
+printf (" Digite a populacao da cidade: ");
+scanf ("%d", &populacao);
+printf (" Digite area da cidade! ");
+scanf ("%d", &area);
+printf (" Digite o PIB da cidade! ");
+scanf ("%lf", &pib);
+
+
